8610b844a0462b0ace23361994263a9fc06a8f21
PartyParrot <1405974400@qq.com>
2021-11-06 15:25:01 +0800

    EasyPy现在支持逻辑判断了！！！！！

--- EasyScript/EasyRun.py
+++ EasyScript/EasyRun.py
@@ -6,22 +6,26 @@ from EasyScript.EasyContext import *
 from EasyScript.EasySymbolTable import *
 
 global_symbol_table = SymbolTable()
-global_symbol_table.set('null', Number(0))
+global_symbol_table.set('NULL', Number(0))
+global_symbol_table.set('TRUE', Number(1))
+global_symbol_table.set('FALSE', Number(0))
 
 
 def run(fn, text):
     # 生成 Token
     lexers = Lexer(fn, text)
     tokens, error = lexers.make_tokens()
-    if error: return None, error
+    if error: 
+        return None, error
     
     # 生成 AST
     parser = Parser(tokens)
     ast = parser.parse()
-    if ast.error:return None,ast.error
+    if ast.error:
+        return None, ast.error
     
     
-    interpreter =Interpreter()
+    interpreter = Interpreter()
     context = Context('<program>')
     context.symbol_table = global_symbol_table
     result = interpreter.visit(ast.node, context)
--- EasyScript/EasyTokens.py
+++ EasyScript/EasyTokens.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 import string
 
-
 # CONSTANTS 常量
 
 # 数字
@@ -12,8 +11,6 @@ LETTERS = string.ascii_letters
 
 # 数字 + 字母
 LETTERS_DIGITS = LETTERS + DIGITS
-
-
 
 # TOKEN
 
@@ -29,28 +26,37 @@ TT_POW = 'POW'
 TT_EQ = 'EQ'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
+TT_EE = 'EE'
+TT_NE = 'NE'
+TT_LT = 'LT'
+TT_GT = 'GT'
+TT_LTE = 'LTE'
+TT_GTE = 'GTE'
 TT_EOF = 'EOF'
 
-KEYWORDS =[
-    'VAR'
-]
+KEYWORDS = ['VAR',
+            'AND',
+            'NOT',
+            'OR',
+            ]
+
 
 class Token:
-    def __init__(self, type_, value = None, pos_start=None, pos_end = None):
+    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
         self.type = type_
         self.value = value
-        
+
         if pos_start:
             self.pos_start = pos_start.copy()
             self.pos_end = pos_start.copy()
             self.pos_end.advance()
-        
+
         if pos_end:
             self.pos_end = pos_end
-    
+
     def matches(self, type_, value):
         return self.type == type_ and self.value == value
-    
+
     def __repr__(self):
         if self.value:
             return f'{self.type}:{self.value}'
