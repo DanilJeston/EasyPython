2ceb761e3f15f3dec23314dc76efa569a2af72df
SANS <2100970361@qq.com>
2021-11-19 21:19:50 +0800

    Nothing.

--- 源代码/EasyShell.py
+++ 源代码/EasyShell.py
@@ -10,9 +10,8 @@ import datetime
 import platform
 
 
-def shell():
-    Ctrl_C_Press = 0             # 记录Ctrl + C按下了几次
-    Ctrl_Z_Press = 0           # 记录Ctrl + Z按下了几次
+def get_release_time(release_year, release_months, release_day, release_hour,
+                     release_min, release_sec):
     release_months_dict = {
         '1': "Jan",
         '2': "Feb",
@@ -26,14 +25,8 @@ def shell():
         '10': "Oct",
         '11': "Nov",
         '12': "Dec"
-    }                                 # 创建月份字典
-    version = "0.1.1"                 # 版本
-    release_months = 11               # 月份
-    release_day = 6                   # 天
-    release_year = 2021               # 年
-    release_hour = 16                 # 小时
-    release_min = 26                  # 分钟
-    release_sec = 40                  # 秒
+    }  # 创建月份字典
+
     # 发布日期↓
     release_date_time = "{months} {day} {year}, {hour}:{min}:{sec}".format(
         months=release_months_dict[str(release_months)],
@@ -42,15 +35,32 @@ def shell():
         hour=str(release_hour),
         min=str(release_min),
         sec=str(release_sec))
+    return release_date_time
+
+
+def shell():
+    Ctrl_C_Press = 0  # 记录Ctrl + C按下了几次
+    Ctrl_Z_Press = 0  # 记录Ctrl + Z按下了几次
+    version = "0.1.2"  # 版本
+    release_months = 11  # 月份
+    release_day = 6  # 天
+    release_year = 2021  # 年
+    release_hour = 16  # 小时
+    release_min = 26  # 分钟
+    release_sec = 40  # 秒
+    commit_hash_code = "" # 前七位哈希值
+    release_date_time = get_release_time(release_year, release_months,
+                                         release_day, release_hour,
+                                         release_min, release_sec)
     # start_help_info = '"help", "copyright", "credits" or "license"'
     # 启动时显示的帮助信息↓
     start_help_info = '"help", "copyright"'
     # 输出启动信息↓
-    print("""
-EasyPy(EasyPython) {version} ,Released in {date_time}, Run EasyPy on {system_version1}, {system_version2}.
+    print(f"""
+EasyPy(EasyPython) {version} ({commit_hash_code}:{version}), Released in {release_date_time}, Run EasyPy on {sys.platform}, {platform.platform()}.
 Type {start_help_info} for more information.
 Source Code:https://gitee.com/ky-studio/EasyPython
-    """.format(version=version, date_time=release_date_time, system_version1=platform.platform(), system_version2=sys.platform, start_help_info=start_help_info))
+    """)
     while True:
         # 用try是为了检测Ctrl + C
         try:
@@ -74,7 +84,8 @@ Source Code:https://gitee.com/ky-studio/EasyPython
                     year = "2021"
                 else:
                     year = '2021 - ' + str(datetime.datetime.now().year)
-                print("Copyright © {year} , {author}. All rights reserved".format(year=year, author=author))  # 版权信息提示
+                print("Copyright © {year} , {author}. All rights reserved".
+                      format(year=year, author=author))  # 版权信息提示
             elif command == 'rules':
                 pass
             else:
@@ -88,7 +99,9 @@ Source Code:https://gitee.com/ky-studio/EasyPython
 
         except KeyboardInterrupt:
             if Ctrl_C_Press < 1:
-                print("\n\"Ctrl + C\" has been pressed. if you want exit, please press the key again.")
+                print(
+                    "\n\"Ctrl + C\" has been pressed. if you want exit, please press the key again."
+                )
                 Ctrl_C_Press += 1
             else:
                 print("\nGoodbye, Thank you for using.")
@@ -96,7 +109,9 @@ Source Code:https://gitee.com/ky-studio/EasyPython
 
         except EOFError:
             if Ctrl_Z_Press < 1:
-                print("\n\"Ctrl + Z\" has been pressed. if you want exit, please press the key again.")
+                print(
+                    "\n\"Ctrl + Z\" has been pressed. if you want exit, please press the key again."
+                )
                 Ctrl_Z_Press += 1
             else:
                 print("\nGoodbye, Thank you for using.")
